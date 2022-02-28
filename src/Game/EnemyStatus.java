9edcfa1f8503d7ae3b9968e01b498a78cb38f541
aliamanticarpa <aliamanticarpa@mail.ru>
2022-02-28 21:46:15 +0300

    бэкапп

---
+++ src/Game/EnemyStatus.java
@@ -0,0 +1,6 @@
+public class EnemyStatus extends Status{
+
+    public EnemyStatus(GameObject object,int time) {
+        super(object,time);
+    }
+}
---
+++ src/Game/TowerStatus.java
@@ -0,0 +1,6 @@
+public class TowerStatus extends Status {
+
+    public TowerStatus(GameObject object,int time) {
+        super(object,time);
+    }
+}
